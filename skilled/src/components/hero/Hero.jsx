dd1761d5e12a1be4cf8b981673f4a90791e1d4de
Russell Crooms <russella.crooms@gmail.com>
2024-06-06 23:15:13 -0500

    I've finsihed adding all of the elements to the page. Now I am experimenting with the position of all of the elements. The goal is to have this look as close to the mockup as possible.

--- skilled/src/components/hero/Hero.jsx
+++ skilled/src/components/hero/Hero.jsx
@@ -1,11 +1,28 @@
-import './Hero.css';
+import "./Hero.css";
+import hero_image_desktop from "../../assets/image-hero-desktop.png";
 
 const Hero = () => {
   return (
-    <div>
-
+    <div className="hero-container">
+      <div className="hero-container-left">
+        <div className="hero-header">
+          <h2>Maximize skill, <br></br>minimize budget</h2>
+        </div>
+        <div className="hero-para">
+          <p>
+            Our modern courses across a range of in-demand<br></br>skills will give you
+            the knowledge you need to live<br></br>the life you want.
+          </p>
+        </div>
+        <div className="hero-button">
+            <button>Get Started</button>
+        </div>
+      </div>
+      <div className="hero-container-right">
+        <img src={hero_image_desktop} alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
