9444e059296999d24d491afdf7bdf18a2350add9
Russell Crooms <russella.crooms@gmail.com>
2024-06-06 21:48:19 -0500

    Created Hero component, and linked the stylesheet.

--- skilled/src/App.jsx
+++ skilled/src/App.jsx
@@ -1,10 +1,11 @@
-
+import Hero from './components/hero/Hero'
 import NavBar from './components/navBar/NavBar'
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Hero />
     </div>
   )
 }
